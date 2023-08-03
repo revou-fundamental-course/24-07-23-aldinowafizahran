ce53581d4e653e628e0ca5bd483de4272ab73fc3
LAPTOP-JT68GQP3\HP <aldiwafi09@gmail.com>
2023-08-03 20:36:26 +0700

    try commit12

--- js/script.js
+++ js/script.js
@@ -38,19 +38,3 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 2000); 
 }
-
-// let index = 0;
-// displayImages();
-// function displayImages() {
-//   let i;
-//   const images = document.getElementsByClassName("image");
-//   for (i = 0; i < images.length; i++) {
-//     images[i].style.display = "none";
-//   }
-//   index++;
-//   if (index > images.length) {
-//     index = 1;
-//   }
-//   images[index-1].style.display = "block";
-//   setTimeout(displayImages, 2000); 
-// }
